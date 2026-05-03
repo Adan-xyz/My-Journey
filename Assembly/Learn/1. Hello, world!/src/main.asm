7da753c1b73388c7402a4e7e0175e804ad9d5942
Adanxyz <adansaprudin.xyz@gmail.com>
2026-05-03 20:40:00 +0800

    Add Hello World program in Assembly

--- Assembly/Learn/1. Hello, world!/src/main.asm
+++ Assembly/Learn/1. Hello, world!/src/main.asm
@@ -1,1 +1,15 @@
+; Hello World Program
+ 
+section .data
+    msg db "Hello World!", 10    ; assign msg variable with your message string
+ 
+section .text
+    global  _start
+ 
+_start:
 
+    mov rax, 1      ; invoke SYS_WRITE (kernel opcode 1)
+    mov rdi, 1      ; write to the STDOUT file
+    mov rsi, msg    ; move the memory address of our message string into rsi
+    mov rdx, 13     ; number of bytes to write - one for each letter plus 10 (line feed character)
+    syscall
