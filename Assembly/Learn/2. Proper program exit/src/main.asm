069a8aaa47410bc399dc2ff2b941c1731e054651
Adanxyz <adansaprudin.xyz@gmail.com>
2026-05-03 20:47:44 +0800

    Implement proper program exit

--- Assembly/Learn/2. Proper program exit/src/main.asm
+++ Assembly/Learn/2. Proper program exit/src/main.asm
@@ -1,1 +1,19 @@
+; Hello World Program
+ 
+section .data
+    msg db "Hello World!", 10
+ 
+section .text
+    global  _start
+ 
+_start:
 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
+
+    mov rax, 60     ; invoke SYS_EXIT (kernel opcode 60)
+    xor rdi, rdi    ; return 0 status on exit - 'No Errors'
+    syscall
