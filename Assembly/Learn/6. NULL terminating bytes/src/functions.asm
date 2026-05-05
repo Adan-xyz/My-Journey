915cf46221c1282b4193ea33b36ee36374d99331
Adanxyz <adansaprudin.xyz@gmail.com>
2026-05-05 09:52:14 +0800

    Add string length and print functions in assembly
    Implement functions to calculate string length and print strings.

---
+++ Assembly/Learn/6. NULL terminating bytes/src/functions.asm
@@ -0,0 +1,42 @@
+slen:
+    push rdi
+    mov  rdi, rax
+
+nextchar:
+    cmp byte [rax], 0
+    jz  finished
+    inc rax
+    jmp nextchar
+
+finished:
+    sub rax, rdi
+    pop rdi
+    ret
+
+
+sprint:
+    push rdx
+    push rsi
+    push rdi
+    push rax
+    call slen
+
+    mov rdx, rax
+    pop rax
+
+    mov rsi, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+
+    pop rdi
+    pop rsi
+    pop rdx
+    ret
+
+
+quit:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+    ret
